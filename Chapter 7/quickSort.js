0419b7b58e1c2d240c71d940a200e4df9f3480e7
armandopato <armauv@gmail.com>
2020-07-10 22:23:24 -0500

    Add quickSort

---
+++ Chapter 7/quickSort.js
@@ -0,0 +1,40 @@
+function partition(arr, start, end)
+{
+    const pivot = arr[end];
+    let i = start - 1;
+    for (let j = start; i < end - 1; i++)
+    {
+        if (arr[j] <= pivot)
+        {
+            i++;
+            [arr[i], arr[j]] = [arr[j], arr[i]];
+        }
+    }
+    [arr[i+1], arr[end]] = [arr[end], arr[i+1]];
+    return i+1;
+}
+
+function randomizedPartition(arr, start, end)
+{
+    const x = Math.floor(Math.random() * (end - start + 1)) + start;
+    [arr[x], arr[end]] = [arr[end], arr[x]];
+    return partition(arr, start, end);
+}
+
+function quickSortHelper(arr, start, end)
+{
+    if (start < end)
+    {
+        const pivot = randomizedPartition(arr, start, end);
+        quickSortHelper(arr, start, pivot-1);
+        quickSortHelper(arr, pivot+1, end);
+    }
+}
+
+function quickSort(arr)
+{
+    const copy = [...arr];
+    quickSortHelper(copy, 0, copy.length - 1);
+    return copy;
+}
+
